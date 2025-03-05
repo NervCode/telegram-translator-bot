9fd4c2aa5bba3b484959fd22dcba3c1583df3e91
nerv <tsepelev.online@gmail.com>
2025-03-05 19:28:47 +0900

    .

--- app/handlers.py
+++ app/handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery, BufferedInputFile
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
 
